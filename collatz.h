31e4dbb4e58a28a86985c2c3181763f614539f68
Joshua R <111922365+Jrusli@users.noreply.github.com>
2024-09-30 01:03:18 -0700

    Update collatz.h

--- collatz.h
+++ collatz.h
@@ -3,8 +3,8 @@
 
 #include "cache.h" 
 
-// Declaration of the Collatz function
+
 int collatz_steps(CacheNumber num);
-int collatz_steps_cached(CacheNumber num); // Function declaration for the cached version
+int collatz_steps_cached(CacheNumber num);
 
 #endif
